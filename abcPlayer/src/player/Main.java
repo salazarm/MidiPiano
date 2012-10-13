e94e5e23cd6b7e8014cd8035a26bb63e3f58e985
willcu <willcu@mit.edu>
2012-10-12 21:14:05 -0400

    Starting code

---
+++ abcPlayer/src/player/Main.java
@@ -0,0 +1,20 @@
+package player;
+
+/**
+ * Main entry point of your application.
+ */
+public class Main {
+
+	/**
+	 * Plays the input file using Java MIDI API and displays
+	 * header information to the standard output stream.
+	 * 
+	 * <p>Your code <b>should not</b> exit the application abnormally using
+	 * System.exit()</p>
+	 * 
+	 * @param file the name of input abc file
+	 */
+	public static void play(String file) {
+		// YOUR CODE HERE
+	}
+}
